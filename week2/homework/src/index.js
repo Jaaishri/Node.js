4fb42af8b48955eca03b752d8fae0956e6bff44b
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2019-11-26 12:31:33 +0100

    Updated

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,41 @@
-'use strict';
+
 
 // TODO: Write the homework code in this file
+"use strict";
+
+const helpList = {
+  help: "show the help commands",
+  list: "list the to dos",
+  add: "add to to do list",
+  remove: "remove one or more from the list",
+  reset: "reset all the items in the to dos list"
+};
+
+let toDosList = ["go", "come", "buy"];
+
+if (process.argv[2] === "help" || !process.argv[2])
+{
+  console.log(helpList);
+}
+else if (process.argv[2] === "list")
+{
+  console.log(toDosList);
+}
+else if (process.argv[2] === "add") 
+{
+  toDosList.push(process.argv[3]);
+ 
+  console.log(toDosList);
+} 
+else if (process.argv[2] === "remove")
+{
+  toDosList.splice(process.argv[3], 1);
+  
+  console.log(toDosList);
+}
+else if (process.argv[2] === "reset") {
+  
+  toDosList = [];
+  console.log("To dos is empty");
+}
+
