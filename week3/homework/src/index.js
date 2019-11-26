46c18e6f6b8371ff8e5c09f9483956863246a621
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2019-11-26 21:22:31 +0100

    Update index.js

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+'use strict';
+
+// CRUD actions
+module.exports = {
+  createTodo: require('./createTodo'),
+  readTodos: require('./readTodos'),
+  updateTodo: require('./updateTodo'),
+  deleteTodo: require('./deleteTodo'),
+  readTodo: require('./readTodo'),
+  clearTodos: require('./clearTodos'),
+  patchTodo: require('./patchTodo'),
+};
